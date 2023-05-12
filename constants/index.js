4797f21a0affe863337962627913288aa945c760
Matt <matt.reiley@sjsu.edu>
2023-05-12 16:09:06 -0700

    temp idel

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 const QUICKNODE_HTTP_URL = "https://nameless-white-violet.ethereum-sepolia.discover.quiknode.pro/ae4454f26da95b2c2aa1064508f201f1772bd04f/";
+const VotingList_Contract_Address = "0xfed9b85b19dFCd4B8292e57E6E51d083d0314DE2";
 
 module.exports = { METADATA_URL, QUICKNODE_HTTP_URL };
